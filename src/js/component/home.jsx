4d1d7c1ace2dffe8a7ad4634c256cdb73bf76d25
jessicabrinegar <jessicabrinegar@outlook.com>
2023-01-16 17:15:19 +0000

    first version of stopwatch. will change to look more like 4geeks version

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Stopwatch from "./stopwatch.jsx";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
+		<div className="text-center h-100">
+			<h1 className="text-center mt-5">Stopwatch</h1>
+			<Stopwatch />
 			<p>
 				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+				<a href="https://github.com/jessicabrinegar">Jessie B</a>, with
 				love!
 			</p>
 		</div>
 	);
 };
-
 export default Home;
+
+// <SecondsCounter seconds={3434} />
+// Create option to countdown from a specified #, with an alert when it gets to 0
